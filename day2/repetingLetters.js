40f2de4e42712ddeea1e6da61944324c8ec58efd
prlaymaity <pralaymaity283@gmail.com>
2025-10-15 15:14:51 +0530

    repeating letters

---
+++ day2/repetingLetters.js
@@ -0,0 +1,32 @@
+// Repeating Letters
+// Create a function that takes a string and returns a string in
+// which each character is repeated once.
+
+// Examples
+// doubleChar("String") ➞ "SSttrriinngg"
+
+// doubleChar("Hello World!") ➞ "HHeelllloo  WWoorrlldd!!"
+
+// doubleChar("1234!_ ") ➞ "11223344!!__  "
+
+let str = "String";
+
+function repeatLetters(str) {
+  let splitWords = str.split("");
+  let store = [];
+
+  splitWords.map((el, idx) => {
+    return store.push(el + el)
+  });
+
+  let result = store.join("");
+  console.log(typeof result);
+  
+
+  return result
+}
+
+console.log(repeatLetters(str));
+
+
+console.log("adsada");

6583be18bc34bfc50c94f6d78893da3a92571f05
prlaymaity <pralaymaity283@gmail.com>
2025-10-15 12:04:11 +0530

    ternary operator

---
+++ day2/condition.js
@@ -0,0 +1,17 @@
+// No Conditionals?
+// Write a function that returns 0 if the input is 1, 
+// and returns 1 if the input is 0.
+
+// Examples
+// flip(1) ➞ 0
+
+// flip(0) ➞ 1
+
+let num = 0
+function flip (num){
+    return (num === 1)? 0 : 1
+
+}
+
+console.log(flip(num));
+ 

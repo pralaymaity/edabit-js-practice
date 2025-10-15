59fe88b17ab9383942d9dbf26f15446f81ddc0cd
prlaymaity <pralaymaity283@gmail.com>
2025-10-15 17:03:47 +0530

    even number with indices

---
+++ day2/evenAll.js
@@ -0,0 +1,28 @@
+/*Even All the Way
+Given an array of numbers, return an array which contains
+all the even numbers in the original array, which also have even indices.
+
+Examples
+getOnlyEvens([1, 3, 2, 6, 4, 8]) ➞ [2, 4]
+
+getOnlyEvens([0, 1, 2, 3, 4]) ➞ [0, 2, 4]
+
+getOnlyEvens([1, 2, 3, 4, 5]) ➞ []*/
+
+let arr = [0, 1, 2, 3, 4]
+
+function evenNumbersIndex (arr){
+
+    let result = []
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 == 0 && i % 2 == 0){
+            result.push(arr[i])
+        }
+    } 
+
+    return result
+
+}
+
+console.log(evenNumbersIndex(arr));

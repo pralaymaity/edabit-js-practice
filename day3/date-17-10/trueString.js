ae9827cf92b387a1919d268cdc5d9a1a31256de4
prlaymaity <pralaymaity283@gmail.com>
2025-10-17 11:45:42 +0530

    true string

---
+++ day3/date-17-10/trueString.js
@@ -0,0 +1,29 @@
+// Is it True?
+// In this challenge you will be given a relation between two numbers,
+// written as a string. Write a function that determines if the relation is true or false.
+
+// Examples
+// isTrue("2=2") ➞ true
+
+// isTrue("8<7") ➞ false
+
+// isTrue("5=13") ➞ false
+
+// isTrue("15>4") ➞ true
+
+let str = "16>7";
+
+function trueString(str) {
+  let splitStr = str.split(">");
+  //return splitStr
+
+  for (let i = 0; i < splitStr.length - 1; i++) {
+    if (Number.parseInt(splitStr[i])  >= Number.parseInt(splitStr[i + 1])) {
+      return true;
+    }
+  }
+
+  return "false"
+}
+
+console.log(trueString(str));

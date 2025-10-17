85546023dbfa740e14310dcd1c4cf0a084a5a874
prlaymaity <pralaymaity283@gmail.com>
2025-10-17 17:04:51 +0530

    phone number format

---
+++ day3/date-17-10/phoneNumberFormat.js
@@ -0,0 +1,25 @@
+/*
+Phone Number Formatting
+Create a function that takes an array of 10 numbers (between 0 and 9) and
+returns a string of those numbers formatted as a phone number (e.g. (555) 555-5555).
+
+Examples
+formatPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) ➞ "(123) 456-7890"
+
+formatPhoneNumber([5, 1, 9, 5, 5, 5, 4, 4, 6, 8]) ➞ "(519) 555-4468"
+
+formatPhoneNumber([3, 4, 5, 5, 0, 1, 2, 5, 2, 7]) ➞ "(345) 501-2527"
+*/
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+
+function phoneNumberFormat(arr) {
+  
+    let str = arr.join("")
+
+
+    return `(${str.slice(0,3)}) ${str.slice(3,6)}-${str.slice(6,10)}`
+}
+
+console.log(phoneNumberFormat(arr));
+

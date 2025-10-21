d255ac972f3eb57a04bdb0e6560459a629bad79c
prlaymaity <pralaymaity283@gmail.com>
2025-10-21 19:37:07 +0530

    symmetric butterfly

--- day4-pattern-21-10/symmetricButterfly.js
+++ day4-pattern-21-10/symmetricButterfly.js
@@ -11,10 +11,46 @@ Result:
 *    *
 */
 
+let num = 3;
 
+function halfDiamond(num) {
+  let str = "";
 
+  for (let i = 0; i < num; i++) {
 
+    for (let j = 0; j <= i; j++){
+        str += "*"
+    }
 
+    for (let j = 0; j < 2 * (num - i - 1); j++) {
+      str += " ";
+    }
 
+    for (let j = 0; j <= i; j++) {
+      str += "*";
+    }
 
+    str += "\n";
+  }
 
+  for(let i = num - 2; i >= 0; i--){
+
+    for (let j = 0; j <= i; j++){
+        str += "*"
+    }
+
+    for(let j = 0;  j < 2 * (num - i -1); j++){
+        str = str + " "
+    }
+
+    for (let j = 0; j <= i; j++){
+        str += "*"
+    }
+
+    str += "\n";
+  }
+
+return str
+}
+
+console.log(halfDiamond(num));
